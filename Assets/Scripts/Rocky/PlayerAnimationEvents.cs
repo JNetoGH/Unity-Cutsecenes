40ed8c49a61df3ca0bdf24034fd3353264209e7a
JNeto <joaomendes15@gmail.com>
2023-12-01 14:26:53 +0000

    spline 1 finished

---
+++ Assets/Scripts/Rocky/PlayerAnimationEvents.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+
+public class PlayerAnimationEvents : MonoBehaviour
+{
+    
+    private Animator _animator;
+
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    public void Spin()
+    {
+        _animator.SetTrigger("spin");
+    }
+    
+}
